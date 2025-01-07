44bad2def40da9231f36cd092d04bfdeb7d068fe
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-01-07 13:55:28 +0100

    Completed the exercises and made a couple of classes in Extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -4,4 +4,21 @@ public class Extension {
     public static String sayHelloWorld() {
         return "Hello, world!";
     }
+
+    public class Car {
+        public String make = "Toyota";
+        public String model = "F5";
+        public int numDoors = 4;
+        public int litres = 50;
+        public String engineType = "VX8";
+        public String color = "yellow";
+        public String regNumber = "FE35343";
+    }
+
+    public class Movie {
+        public String title = "Dune: Part One";
+        public int runtimeMinutes = 155;
+        public int releaseYear = 2021;
+        public String director = "Denis Villeneuve";
+    }
 }
